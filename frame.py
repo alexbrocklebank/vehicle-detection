e1a8dbd69c9c2a7871211a6398e9351f06a53b95
alexbrocklebank <alexbrocklebank@gmail.com>
2017-09-19 20:46:09 -0400

    Download sample images.  Add plot3d.  Comment old unneccessary lines.

--- frame.py
+++ frame.py
@@ -2,11 +2,12 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from mpl_toolkits.mplot3d import Axes3D
 
-image = mpimg.imread('bbox-example-image.jpg')
-#image = mpimg.imread('temp-matching-example-2.jpg')
-templist = ['cutout1.jpg', 'cutout2.jpg', 'cutout3.jpg',
-            'cutout4.jpg', 'cutout5.jpg', 'cutout6.jpg']
+#image = mpimg.imread('bbox-example-image.jpg')
+# image = mpimg.imread('temp-matching-example-2.jpg')
+#templist = ['cutout1.jpg', 'cutout2.jpg', 'cutout3.jpg',
+#            'cutout4.jpg', 'cutout5.jpg', 'cutout6.jpg']
 
 # Define a function that takes an image, a list of bounding boxes,
 # and optional color tuple and line thickness as inputs
@@ -74,8 +75,8 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     return rhist, ghist, bhist, bin_centers, hist_features
 
 
-rh, gh, bh, bincen, feature_vec = color_hist(image, nbins=32,
-                                             bins_range=(0, 256))
+#rh, gh, bh, bincen, feature_vec = color_hist(image, nbins=32,
+#                                             bins_range=(0, 256))
 
 # Plot a figure with all three bar charts
 if rh is not None:
@@ -96,6 +97,55 @@ if rh is not None:
 else:
     print('Your function is returning None for at least one variable...')
 
-bboxes = find_matches(image, templist)
-result = draw_boxes(image, bboxes)
-plt.imshow(result)
+#bboxes = find_matches(image, templist)
+#result = draw_boxes(image, bboxes)
+#plt.imshow(result)
+
+
+def plot3d(pixels, colors_rgb,
+        axis_labels=list("RGB"), axis_limits=[(0, 255), (0, 255), (0, 255)]):
+    """Plot pixels in 3D."""
+
+    # Create figure and 3D axes
+    fig = plt.figure(figsize=(8, 8))
+    ax = Axes3D(fig)
+
+    # Set axis limits
+    ax.set_xlim(*axis_limits[0])
+    ax.set_ylim(*axis_limits[1])
+    ax.set_zlim(*axis_limits[2])
+
+    # Set axis labels and sizes
+    ax.tick_params(axis='both', which='major', labelsize=14, pad=8)
+    ax.set_xlabel(axis_labels[0], fontsize=16, labelpad=16)
+    ax.set_ylabel(axis_labels[1], fontsize=16, labelpad=16)
+    ax.set_zlabel(axis_labels[2], fontsize=16, labelpad=16)
+
+    # Plot pixel values with colors given in colors_rgb
+    ax.scatter(
+        pixels[:, :, 0].ravel(),
+        pixels[:, :, 1].ravel(),
+        pixels[:, :, 2].ravel(),
+        c=colors_rgb.reshape((-1, 3)), edgecolors='none')
+
+    return ax  # return Axes3D object for further manipulation
+
+
+# Read a color image
+img = cv2.imread("000275.png")
+
+# Select a small fraction of pixels to plot by subsampling it
+scale = max(img.shape[0], img.shape[1], 64) / 64  # at most 64 rows and columns
+img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] / scale)), interpolation=cv2.INTER_NEAREST)
+
+# Convert subsampled image to desired color space(s)
+img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
+img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
+img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
+
+# Plot and show
+plot3d(img_small_RGB, img_small_rgb)
+plt.show()
+
+plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+plt.show()
