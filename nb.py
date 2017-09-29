c943ff2780ddc9b30fdcc63699aaecae24bd0646
alexbrocklebank <alexbrocklebank@gmail.com>
2017-09-29 17:30:14 -0400

    Delete previous lesson files.  Add new python files with starter code from lesson.

--- nb.py
+++
@@ -1,5 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-clf.fit(features_train, labels_train)
-pred = clf.predict(features_test)
-accuracy = clf.score(feature_test, labels_test)
