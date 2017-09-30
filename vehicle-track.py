f679d1002133324e43486574ff6d0f9915a8bc24
alexbrocklebank <alexbrocklebank@gmail.com>
2017-09-29 21:58:27 -0400

    Add logic to store pickle files of classifier, parameters, and features/labels.  Add *.pkl to .gitignore

--- vehicle-track.py
+++ vehicle-track.py
@@ -5,6 +5,8 @@ import pickle
 import cv2
 import time
 from skimage.feature import hog
+from sklearn import datasets
+from sklearn.externals import joblib
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
 # NOTE: the next import is only valid
@@ -20,6 +22,7 @@ cars, notcars = load_data()
 # TODO: Sliding Window Function
 
 # Classifier Parameters
+classifier_pickle = 'classifier.pkl'
 color_space = 'HLS'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 9  # HOG orientations
 pix_per_cell = 8  # HOG pixels per cell
@@ -31,54 +34,110 @@ spatial_feat = True  # Spatial features on or off
 hist_feat = True  # Histogram features on or off
 hog_feat = True  # HOG features on or off
 y_start_stop = [450, None]  # Min and max in y to search in slide_window()
-# TODO: Store parameters to pickle
-# TODO: compare pickle to current params to determine re-run of classifier
+write_params = False
+
+# Attempt to load previously stored parameters
+parameters = {}
+try:
+    loaded_params = pickle.load(open('parameters.pkl', 'rb'))
+    print("Parameters loaded from file.")
+except (OSError, IOError) as e:
+    write_params = True
+
+# Store parameters in dictionary
+param_names = ('classifier_pickle', 'color_space', 'orient', 'pix_per_cell',
+               'cell_per_block', 'hog_channel', 'spatial_size', 'hist_bins',
+               'spatial_feat', 'hist_feat', 'hog_feat', 'y_start_stop')
+for i in param_names:
+    if not write_params:
+        if loaded_params[i] != locals()[i]:
+            write_params = True
+    parameters[i] = locals()[i]
+
+# Store dictionary of parameters to pickle
+if write_params:
+    with open('parameters.pkl', 'wb') as f:
+        pickle.dump(parameters, f)
+        print("Parameters written to file.")
 
 # Obtain Features
-car_features = extract_features(cars, color_space=color_space,
-                                spatial_size=spatial_size, hist_bins=hist_bins,
-                                orient=orient, pix_per_cell=pix_per_cell,
-                                cell_per_block=cell_per_block,
-                                hog_channel=hog_channel,
-                                spatial_feat=spatial_feat,
-                                hist_feat=hist_feat, hog_feat=hog_feat)
-notcar_features = extract_features(notcars, color_space=color_space,
-                                   spatial_size=spatial_size,
-                                   hist_bins=hist_bins,
-                                   orient=orient, pix_per_cell=pix_per_cell,
-                                   cell_per_block=cell_per_block,
-                                   hog_channel=hog_channel,
-                                   spatial_feat=spatial_feat,
-                                   hist_feat=hist_feat, hog_feat=hog_feat)
+if (not write_params) and im_a_pickle_morty("car_features.pkl"):
+    car_features = joblib.load("car_features.pkl")
+    print("Car Features loaded from file.")
+else:
+    car_features = extract_features(cars, color_space=color_space,
+                                    spatial_size=spatial_size,
+                                    hist_bins=hist_bins,
+                                    orient=orient, pix_per_cell=pix_per_cell,
+                                    cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel,
+                                    spatial_feat=spatial_feat,
+                                    hist_feat=hist_feat, hog_feat=hog_feat)
+    joblib.dump(car_features, "car_features.pkl")
+    print("Car Features written to file.")
+
+if (not write_params) and im_a_pickle_morty("notcar_features.pkl"):
+    notcar_features = joblib.load("notcar_features.pkl")
+    print("Notcar Features loaded from file.")
+else:
+    notcar_features = extract_features(notcars, color_space=color_space,
+                                       spatial_size=spatial_size,
+                                       hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell,
+                                       cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel,
+                                       spatial_feat=spatial_feat,
+                                       hist_feat=hist_feat, hog_feat=hog_feat)
+    joblib.dump(notcar_features, "notcar_features.pkl")
+    print("Notcar Features written to file.")
 
 # Normalize/Scale Features
-X = np.vstack((car_features, notcar_features)).astype(np.float64)
-# Fit a per-column scaler
-X_scaler = StandardScaler().fit(X)
-# Apply the scaler to X
-scaled_X = X_scaler.transform(X)
-# Define the labels vector
-y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+scaled_X = False
+y = False
+if not write_params:
+    if im_a_pickle_morty("X_scaled.pkl"):
+        scaled_X = joblib.load("X_scaled.pkl")
+        print("Scaled X loaded from file.")
+    if im_a_pickle_morty("y_labels.pkl"):
+        y = joblib.load("y_labels.pkl")
+        print("Y Labels loaded from file.")
+if (not scaled_X) or (not y):
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+    joblib.dump(scaled_X, "X_scaled.pkl")
+    print("Scaled X written to file.")
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+    joblib.dump(y, "y_labels.pkl")
+    print("Y Labels written to file.")
 # Split up data into randomized training and test sets
 rand_state = np.random.randint(0, 100)
 X_train, X_test, y_train, y_test = train_test_split(
     scaled_X, y, test_size=0.2, random_state=rand_state)
-# TODO: Pickle image data?
 
 # Create Classifier
 svc = LinearSVC()
 
-# Train Clasifier
-# Check the training time for the SVC
-t = time.time()
-svc.fit(X_train, y_train)
-t2 = time.time()
-print(round(t2-t, 2), 'Seconds to train SVC...')
-# Check the score of the SVC
-print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-# Check the prediction time for a single sample
-t = time.time()
-# TODO: Can you pickle a classifier?
+# Train Clasifier if it hasn't already been stored & parameters are unchanged
+if (not write_params) and im_a_pickle_morty(classifier_pickle):
+    svc = joblib.load(classifier_pickle)
+    print("Classifier loaded...")
+else:
+    # Check the training time for the SVC
+    t = time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t = time.time()
+    # Pickle the classifier for re-use
+    joblib.dump(svc, classifier_pickle)
+    print("Classifier saved.")
 
 # TODO: Receive Frame
 
